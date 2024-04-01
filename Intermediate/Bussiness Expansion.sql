512be132802de740bf5ab29e8948e37d6584edd4
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-04-01 21:13:26 +0500

    Add files via upload

---
+++ Intermediate/Bussiness Expansion.sql
@@ -0,0 +1,7 @@
+SELECT a.id, ua.first_name, a.last_name, c.id, c.customer_name, COUNT(*)
+FROM contact contacts
+INNER JOIN customer c ON c.id=contacts.customer_id
+INNER JOIN user_account a ON a.id=con.user_account_id
+GROUP BY a.id, a.first_name, a.last_name, c.id, c.customer_name
+HAVING COUNT(*)>1
+ORDER BY a.id
